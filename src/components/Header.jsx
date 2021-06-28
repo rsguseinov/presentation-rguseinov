27e0cb1001d20729a5ec6dcbf2d8a309e32bfb35
HitenG2 <rs.guseinov@gmail.com>
2021-06-28 20:45:17 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://w7.pngwing.com/pngs/661/898/png-transparent-react-javascript-library-web-development-vue-js-funding-icon-logo-symmetry-web-development.png"></img>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://i.ytimg.com/vi/6SAgyv2E7S8/maxresdefault.jpg"></img>
+      </div>
+      <div>ava + desk</div>
+      <div>
+        My posts
+        <div>new post</div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
